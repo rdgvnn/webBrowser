0acacd85e53075dd06f7b80bfade08628077ea71
Renato Degiovanni <rdgvnn@gmail.com>
2017-08-17 18:54:26 -0300

    WebBrowser
    Simple web browser

--- webBrowser/ViewController.swift
+++ webBrowser/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, UIWebViewDelegate
+    {
 
+    @IBOutlet weak var mySearchBar: UISearchBar!
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    @IBAction func back(_ sender: Any)
+    {
+        if myWebView.canGoBack
+        {
+            myWebView.goBack()
+        }
+    }
+
+    @IBAction func refresh(_ sender: Any)
+    {
+        myWebView.reload()
+    }
+    
+    @IBAction func next(_ sender: Any)
+    {
+        if myWebView.canGoForward
+        {
+            myWebView.goForward()
+        }
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        mySearchBar.resignFirstResponder()
+        
+        if let url = URL(string: mySearchBar.text!)
+        {
+            myWebView.loadRequest(URLRequest(url: url))
+        }
+        else
+        {
+            print("Error")
+        }
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myWebView.loadRequest(URLRequest(url: URL(string: "https://www.google.com.br")!))
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
